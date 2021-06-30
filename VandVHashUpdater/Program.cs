fb840ef3a5d8163ae13ab24650309a31c3b60c8c
ruaduck <sales@uoguyz.net>
2021-06-30 06:07:42 -0600

    Hasher
    Hasher

---
+++ VandVHashUpdater/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace VandVHashUpdater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        private static void OutputToCSV()
+        {
+            string newFileName = $@"{Application.StartupPath}\Hashes.csv";
+            foreach (var tile in bannedtile)
+            {
+                string mytext = $"{tile.Color},{tile.Tile},{tile.X},{tile.Y},{tile.Z}{Environment.NewLine}";
+                File.AppendAllText(newFileName, mytext);
+            }
+        }
+        public struct MyHash
+        {
+
+            public string File;
+            public HashCode X;
+            public MD5CryptoServiceProvider Y;
+
+
+        }
+
+    }
+}
